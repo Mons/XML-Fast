b23c06324fa59418af19761389063807fca46d9f
Mons Anderson <mons@cpan.org>
2010-12-01 19:04:12 +0300

    upgrade

--- lib/XML/Fast.pm
+++ lib/XML/Fast.pm
@@ -8,7 +8,7 @@ use Encode;
 use base 'Exporter';
 our @EXPORT_OK = our @EXPORT = qw( xml2hash );
 
-our $VERSION = '0.09_01';
+our $VERSION = '0.10';
 
 use XSLoader;
 XSLoader::load('XML::Fast', $VERSION);
